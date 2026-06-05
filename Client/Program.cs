550cf8b9844caf3f9b3778d93e386a132460ac20
sochi6682-sudo <sochi6682@gmail.com>
2026-06-05 11:21:45 +0900

    API作成

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace Client;
+﻿using System.Reflection.Metadata;
+
+namespace Client;
 
 internal class Program
 {
-    static void Main(string[] args)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("クライアント起動 GET送信開始");
+        HttpClient client = new HttpClient();
+
+        var response = await client.GetAsync("http://localhost:8080/");
+        string result = await response.Content.ReadAsStringAsync();
+        Console.WriteLine(result);
+
+
     }
 }
