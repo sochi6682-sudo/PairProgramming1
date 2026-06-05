d3ae21abf94b2ecb47da01f68282580a89c7a488
yuyanagano-youth <ynagano@youtheng.co.jp>
2026-06-05 14:54:11 +0900

    コメント追記

--- Client/obj/Debug/net10.0/Client.AssemblyInfo.cs
+++ Client/obj/Debug/net10.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48fabd6adb63920ae00eb93cd3b54fb26cd45791")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02dae46a242930c2c42589e7c6721bde49e92443")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
